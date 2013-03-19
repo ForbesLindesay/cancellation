de940a2c67444534de88df9aa4e9ca1c0cfd1fcf
ForbesLindesay <forbes@lindesay.co.uk>
2013-03-19 02:57:24 +0000

    Update dependencies

--- test/test.js
+++ test/test.js
@@ -1,29 +1,24 @@
-var promise = require('promises-a');
+require('mocha-as-promised')();
+var promise = require('promise');
 var tokenSource = require('../');
 var assert = require('better-assert');
 
 function delay(timeout, cancellationToken) {
   cancellationToken = cancellationToken || tokenSource.empty;
-  var def = promise();
-  setTimeout(function () {
-    def.fulfill();
-  }, timeout);
-  cancellationToken
-    .onCancelled(function (reason) {
-      def.reject(reason);
-    });
-  return def.promise;
+  return promise(function (resolve, reject) {
+    setTimeout(resolve, timeout);
+    cancellationToken.onCancelled(reject);
+  });
 }
 function delay2(timeout, cancellationToken) {
   cancellationToken = cancellationToken || tokenSource.empty;
-  var def = promise();
-  setTimeout(function () {
-    def.fulfill();
-  }, timeout);
-  setTimeout(function () {
-    if (cancellationToken.isCancelled()) 
-      def.reject(new Error('Operation Cancelled'));
-  }, timeout / 4);
+  return promise(function (resolve, reject) {
+    setTimeout(resolve, timeout);
+    setTimeout(function () {
+      if (cancellationToken.isCancelled()) 
+        reject(new Error('Operation Cancelled'));
+    }, timeout / 4);
+  });
   return def.promise;
 }
 function delay3(timeout, cancellationToken) {
@@ -54,96 +49,89 @@ function cascade(cancellationToken) {
 }
 
 describe('Default token source', function () {
-  it('doesn\'t get cancelled', function (done) {
-    delay(10)
-      .done(done, function (reason) {
-        if (reason) done(reason);
-        else done(new Error('Should not have been cancelled.'));
-      });
+  it('doesn\'t get cancelled', function () {
+    return delay(10);
   });
 });
 
 describe('Cancelling with a token', function () {
-  it('rejects the promise in the next turn of the event loop', function (done) {
+  it('rejects the promise in the next turn of the event loop', function () {
     var source = tokenSource();
     var waitedTillNextTurn = false;
     var timeout;
-    delay(20000, source.token)
-      .done(function () {
-        done(new Error('Should\'ve been cancelled'));
+    source.cancel('Test Cancel');
+    waitedTillNextTurn = true;
+    timeout = setTimeout(function () {
+      throw new Error('Didn\'t cancel fast enough');
+    }, 100);
+
+    return delay(20000, source.token)
+      .then(function () {
+        throw new Error('Should\'ve been cancelled');
       }, function (reason) {
         assert(waitedTillNextTurn);
         assert(reason instanceof Error);
         assert(reason.message === 'Test Cancel');
         assert(reason.code === 'OperationCancelled');
         clearTimeout(timeout);
-        done();
       });
-    source.cancel('Test Cancel');
-    waitedTillNextTurn = true;
-    timeout = setTimeout(function () {
-      done(new Error('Didn\'t cancel fast enough'));
-    }, 10);
   });
 });
 
 describe('Polling for cancellation', function () {
   describe('using `.isCancelled()`', function () {
-    it('works', function (done) {
+    it('works', function () {
       var source = tokenSource();
       var timeout;
-      delay2(40, source.token)
-        .done(function () {
-          done(new Error('Should\'ve been cancelled'));
-        }, function (reason) {
-          clearTimeout(timeout);
-          done();
-        });
       source.cancel('Test Cancel');
       timeout = setTimeout(function () {
-        done(new Error('Didn\'t cancel fast enough'));
+        throw new Error('Didn\'t cancel fast enough');
       }, 30);
+      return delay2(40, source.token)
+        .then(function () {
+          throw new Error('Should\'ve been cancelled');
+        }, function (reason) {
+          clearTimeout(timeout);
+        });
     });
   });
   describe('using `.throwIfCancelled()`', function () {
-    it('works', function (done) {
+    it('works', function () {
       var source = tokenSource();
       var timeout;
-      delay3(40, source.token)
-        .done(function () {
-          done(new Error('Should\'ve been cancelled'));
-        }, function (reason) {
-          clearTimeout(timeout);
-          done();
-        });
       source.cancel('Test Cancel');
       timeout = setTimeout(function () {
-        done(new Error('Didn\'t cancel fast enough'));
+        throw new Error('Didn\'t cancel fast enough');
       }, 30);
+      return delay3(40, source.token)
+        .then(function () {
+          throw new Error('Should\'ve been cancelled');
+        }, function (reason) {
+          clearTimeout(timeout);
+        });
     });
   });
 });
 
 describe('Cascading cancellation', function () {
-  it('works', function (done) {
+  it('works', function () {
     var source = tokenSource();
     var waitedTillNextTurn = false;
     var timeout;
-    cascade(source.token)
-      .done(function () {
-        done(new Error('Should\'ve been cancelled'));
+    source.cancel('Test Cancel');
+    waitedTillNextTurn = true;
+    timeout = setTimeout(function () {
+      throw new Error('Didn\'t cancel fast enough');
+    }, 20);
+    return cascade(source.token)
+      .then(function () {
+        throw new Error('Should\'ve been cancelled');
       }, function (reason) {
         assert(waitedTillNextTurn);
         assert(reason instanceof Error);
         assert(reason.message === 'Test Cancel');
         assert(reason.code === 'OperationCancelled');
         clearTimeout(timeout);
-        done();
       });
-    source.cancel('Test Cancel');
-    waitedTillNextTurn = true;
-    timeout = setTimeout(function () {
-      done(new Error('Didn\'t cancel fast enough'));
-    }, 10);
   });
 });
